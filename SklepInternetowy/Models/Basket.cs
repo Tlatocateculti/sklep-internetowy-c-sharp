3bb8429ce2456179d34aa841e822c8042bd0be10
Liam Poprovsky <tlatocateculti@gmail.com>
2022-06-22 07:59:04 +0000

    Baza

---
+++ SklepInternetowy/Models/Basket.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SklepInternetowy.Models
+{
+    public class Basket
+    {
+        Int64 _id;
+        public Int64 ID
+        {
+            get => _id;
+
+            set => _id = value;           
+        }
+    
+        /*public Int64 ID { get {
+                return ID - 2;
+            }
+            set {
+                this.ID = 12;
+            }
+        }*/
+        public List<string> Items { get; set; }
+        public string Order { get; set; }
+        public string BasketName {get; set;}
+    }
+
+
+}
---
+++ SklepInternetowy/Pages/Index.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SklepInternetowy.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        private int _wartosc = 13;
+        private int innaZmienna;
+
+        public int Wartosc
+        {
+            get
+            {
+                return _wartosc;
+            }
+            set => _wartosc =  (value > -1) ?  value : 0;
+        }
+
+        protected int Konto { get;  }
+
+        /*public void setWartosc(int w) { 
+            if (w > 15) 
+                _wartosc = -1; 
+            else
+                _wartosc = w;  
+        }
+
+        public int getWartosc() { return (_wartosc<0) ? 0 : _wartosc; }*/
+
+        public Models.Basket Basket { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            Konto = 13;
+        }
+
+        public void OnGet()
+        {
+            Basket = new Models.Basket();
+            Basket.ID = 12;
+           // Console.WriteLine(Basket.ID);
+
+        }
+
+        public string DodajTekst()
+        {
+            return DodajTekst(0);
+        }
+
+        public string DodajTekst(int v)
+        {
+            return "<p>Tekst z funkcji"+ v +"</p>";
+        }
+    }
+}
